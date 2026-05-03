4abf1fb88ea4a32b35277be554c81183941481ae
Khin Su Wai <khinsuwai009@gmail.com>
2026-05-03 19:45:31 +0700

    added async wrapper

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,27 +1,49 @@
-const Bootcamp = require('../models/Bootcamp');
+const Bootcamp = require("../models/Bootcamp");
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("../middleware/async");
 
-exports.getBootcamps = (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-}
+exports.getBootcamps = asyncHandler(async (req, res) => {
+  const bootcamps = await Bootcamp.find();
+  res
+    .status(200)
+    .json({ success: true, count: bootcamps.length, data: bootcamps });
+});
 
-exports.getBootcamp = (req, res) => {
-  res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
-}
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
 
-exports.createBootcamp = async(req, res, next) => {
-  try {
-    const bootcamp = await Bootcamp.create(req.body);
-    res.status(201).json({ success: true, data: bootcamp });
-  } catch (err) {
-    next(err);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404),
+    );
   }
-}
+  res.status(200).json({ success: true, data: bootcamp });
+});
 
-exports.updateBootcamp = (req, res) => {
-  res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-}
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({ success: true, data: bootcamp });
+});
 
-exports.deleteBootcamp = (req, res) => {
-  res.status(200).json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-}
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404),
+    );
+  }
+  res.status(200).json({ success: true, data: bootcamp });
+});
 
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404),
+    );
+  }
+  res.status(200).json({ success: true, data: {} });
+});
--- middleware/error.js
+++ middleware/error.js
@@ -1,29 +1,25 @@
-const ErrorResponse = require('../utils/ErrorResponse');
+const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
-  let error = { ...err, message: err.message };
-
-  // Mongoose duplicate key
-  if (err.code === 11000) {
-    const field = Object.keys(err.keyValue)[0];
-    error = new ErrorResponse(`Duplicate value for field: ${field}`, 400);
+  let error = { ...err };
+  error.message = err.message;
+  console.log(err.stack.red);
+  console.log("err", err);
+  if (err.name === "CastError") {
+    const message = `Bootcamp not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
   }
-
-  // Mongoose validation error
-  if (err.name === 'ValidationError') {
-    const message = Object.values(err.errors).map(e => e.message).join(', ');
+  if (err.code === 11000) {
+    const message = "Duplicate field value entered";
     error = new ErrorResponse(message, 400);
   }
-
-  // Mongoose bad ObjectId
-  if (err.name === 'CastError') {
-    error = new ErrorResponse(`Resource not found with id of ${err.value}`, 404);
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
-
-  res.status(error.statusCode || 500).json({
-    success: false,
-    error: error.message || 'Server Error',
-  });
+  res
+    .status(error.statusCode || 500)
+    .json({ success: false, error: error.message || "Server Error" });
 };
 
 module.exports = errorHandler;
